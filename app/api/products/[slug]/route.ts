197f6ef35a60b6d0abdb3c249dda7e5d9ed5fc4e
Özkan BİLGİN <o9616557@gmail.com>
2026-05-19 20:00:48 +0300

    loading sayfasi eklendi ve duzeltmeler yapildi

--- app/api/products/[slug]/route.ts
+++ app/api/products/[slug]/route.ts
@@ -1,24 +1,22 @@
 import { NextResponse } from "next/server";
+import clientPromise from "@/lib/mongodb"; // MongoDB köprümüzü bağlıyoruz
 
 export async function GET(
   request: Request,
-  context: { params: Promise<{ slug: string }> } // Yeni Next.js kuralı
+  context: { params: Promise<{ slug: string }> }
 ) {
   try {
     const { slug } = await context.params;
-    const CK = "ck_6ef66adad9ec356716cc40a803f4669e4c30006b";
-    const CS = "cs_95b1791dad078934610a39930ac3e49da04a6efc";
-    const SITE_URL = "https://bilginpcmarket.com";
+    
+    // Veritabanına bağlan
+    const client = await clientPromise;
+    const db = client.db("bilginpcmarket"); // Veritabanı adın
+    
+    // "products" koleksiyonunda slug'ı eşleşen ürünü bul
+    const urun = await db.collection("products").findOne({ slug: slug });
 
-    const res = await fetch(
-      `${SITE_URL}/wp-json/wc/v3/products?slug=${slug}&consumer_key=${CK}&consumer_secret=${CS}`,
-      { next: { revalidate: 86400 } }
-    );
-
-    const data = await res.json();
-
-    if (res.ok && data && data.length > 0) {
-      return NextResponse.json(data[0]);
+    if (urun) {
+      return NextResponse.json(urun);
     }
 
     return NextResponse.json({ error: "Ürün bulunamadı" }, { status: 404 });
