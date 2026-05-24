35c5d6d6d184af84f362b7cf5f5363a202e99492
Özkan BİLGİN <o9616557@gmail.com>
2026-05-24 18:19:47 +0300

    Iptal mesaji rengi yumusatildi

---
+++ app/favorilerim/loading.tsx
@@ -0,0 +1,8 @@
+export default function Loading() {
+  return (
+    <div className="flex items-center justify-center min-h-screen bg-white">
+      {/* 🚀 ŞEFİM: Ekranda dönecek olan şık ve asil siyah yüklenme halkası */}
+      <div className="w-16 h-16 border-4 border-gray-200 border-t-gray-800 rounded-full animate-spin"></div>
+    </div>
+  );
+}
