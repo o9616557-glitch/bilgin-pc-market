0ff0276c87e21cfcb29170e5eb7b00f5db0f136e
Özkan BİLGİN <o9616557@gmail.com>
2026-05-27 17:51:59 +0300

    Sefimden hizli guncelleme

---
+++ guncelle.bat
@@ -0,0 +1,16 @@
+@echo off
+color 0B
+echo ==========================================
+echo 🚀 SEFIM, KODLAR GITHUB'A UCURULUYOR...
+echo ==========================================
+echo.
+
+git add .
+git commit -m "Sefimden hizli guncelleme"
+git push
+
+echo.
+echo ==========================================
+echo ✅ ISLEM KUSURSUZ TAMAMLANDI!
+echo ==========================================
+timeout /t 3 >nul
