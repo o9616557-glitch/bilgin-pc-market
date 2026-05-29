3a99d2ce94167b15d6df1c78ba78d8817eaf0ca3
Özkan BİLGİN <o9616557@gmail.com>
2026-05-29 21:22:08 +0300

    Sefimden hizli guncelleme

--- app/adreslerim/AdresYoneticisi.tsx
+++ app/adreslerim/AdresYoneticisi.tsx
@@ -97,7 +97,7 @@ export default function AdresYoneticisi({ initialAddresses }: Props) {
   };
 
  return (
-  <div className="max-w-4xl mx-auto w-full relative z-10 mt-7 sm:mt-0">
+  <div className="max-w-4xl mx-auto w-full relative z-10 mt-8 sm:mt-0">
       {/* 🚀 ÜST BAŞLIK VE ÇİZGİ KISMI (Favoriler ile %100 Aynı) */}
       <div className="flex flex-col sm:flex-row sm:items-end sm:justify-between gap-4 border-b border-slate-800 pb-6 mb-10">
         <div>
