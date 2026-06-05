9a1eba3431c6b72df55abef5867d98aa0a9683c2
Özkan BİLGİN <o9616557@gmail.com>
2026-06-05 10:06:56 +0300

    Sefimden hizli guncelleme

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,4 +1,3 @@
-
 import { Toaster } from "react-hot-toast";
 import AuthProvider from "@/components/AuthProvider";
 import type { Metadata } from "next";
@@ -9,6 +8,7 @@ import { CartProvider } from "./CartContext";
 import Footer from "@/components/Footer"; 
 import { CompareProvider } from "./CompareContext";
 import ComparePopup from "./ComparePopup";
+
 const geistSans = Geist({
   variable: "--font-geist-sans",
   subsets: ["latin"],
@@ -30,20 +30,21 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="tr">
-      <body className={geistSans.variable + " " + geistMono.variable + " antialiased min-h-screen flex flex-col bg-[#050814] overflow-x-hidden"}>
-     <Toaster
-  position="top-right"
-  containerStyle={{
-    zIndex: 2147483647
-  }}
-  toastOptions={{
-    style: {
-      background: '#09090b',
-      color: '#fff',
-      border: '1px solid rgba(255,255,255,0.1)'
-    }
-  }}
-/>
+      {/* 🚀 İŞTE BURASI: font-sans eklendi, artık o premium Geist fontu tüm siteye HD kalitesinde basılacak 🚀 */}
+      <body className={`${geistSans.variable} ${geistMono.variable} font-sans antialiased min-h-screen flex flex-col bg-[#050814] overflow-x-hidden`}>
+        <Toaster
+          position="top-right"
+          containerStyle={{
+            zIndex: 2147483647
+          }}
+          toastOptions={{
+            style: {
+              background: '#09090b',
+              color: '#fff',
+              border: '1px solid rgba(255,255,255,0.1)'
+            }
+          }}
+        />
 
         <AuthProvider>
           <CartProvider>
