e7cf14655ae5e0273634a7dea216dee7cccb5c6e
Özkan BİLGİN <o9616557@gmail.com>
2026-06-22 17:21:19 +0300

    Sefimden hizli guncelleme

--- app/giris/page.tsx
+++ app/giris/page.tsx
@@ -109,20 +109,19 @@ export default function GirisPage() {
               ? "Güvenli alışveriş ve sipariş takibi için hesabınıza giriş yapın. Eğer hesabınız yoksa hızlıca kayıt olabilirsiniz."
               : "Hesabınızın güvenliği için ekstra doğrulama gerekiyor."}
           </p>
-
-       {/* SADECE 1. ADIMDA (ŞİFRE EKRANINDA) GÖRÜNEN GOOGLE BUTONU */}
+{/* SADECE 1. ADIMDA (ŞİFRE EKRANINDA) GÖRÜNEN GOOGLE BUTONU */}
        {step === 1 && (
         <div className="w-full mb-6 animate-in fade-in duration-500">
-<button
-  type="button"
-  onClick={() => {
-    toast.loading("Google ile güvenli bağlantı kuruluyor. Lütfen bekleyin...", { duration: 5000 });
-    setTimeout(() => {
-      signIn('google', { callbackUrl: '/' });
-    }, 150);
-  }}
-  className="w-full hover:bg-white/5 border border-white/10 py-3.5..."
->
+          <button
+            type="button"
+            onClick={() => {
+              toast.loading("Google ile güvenli bağlantı kuruluyor. Lütfen bekleyin...", { duration: 5000 });
+              setTimeout(() => {
+                signIn('google', { callbackUrl: '/' });
+              }, 150);
+            }}
+            className="w-full hover:bg-white/5 border border-white/10 py-3.5 rounded-xl flex items-center justify-center gap-3 transition-colors group"
+          >
             <svg className="w-5 h-5 group-hover:scale-110 transition-transform" viewBox="0 0 48 48" xmlns="http://www.w3.org/2000/svg">
               <path fill="#EA4335" d="M24 9.5c3.54 0 6.71 1.22 9.21 3.6l6.85-6.85C35.9 2.38 30.47 0 24 0 14.62 0 6.51 5.38 2.56 13.22l7.98 6.19C12.43 13.72 17.74 9.5 24 9.5z"/>
               <path fill="#4285F4" d="M46.98 24.55c0-1.57-.15-3.09-.38-4.55H24v9.02h12.94c-.58 2.96-2.26 5.48-4.78 7.18l7.73 6c4.51-4.18 7.09-10.36 7.09-17.65z"/>
